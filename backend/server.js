86412751efbb270bb1803724d46cb64350b1985f
Seviko Attalarik <attalarik.handoko@gmail.com>
2024-03-08 19:18:30 +0700

    feat: google cloud deployment

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ app.use(express.json());
 
 app.use(router);
 
-app.listen(3000, () => {
+port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log('Server is running on port 3000');
 });
