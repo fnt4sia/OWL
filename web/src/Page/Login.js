b0c7ba3033eba5f81d706101592c2b615e1cb475
fnt4sia <fitraramadhan668@gmail.com>
2024-03-10 13:22:57 +0700

    add responsive login page

--- web/src/Page/Login.js
+++ web/src/Page/Login.js
@@ -1,34 +1,38 @@
 import googleIcon from '../Assets/google.png'
 import githubIcon from '../Assets/github.png'
+import image from '../Assets/image.png'
 
 export default function LoginPage(){
     return(
-        <div className="py-12 px-4 bg-main h-screen">
-           <div className="flex flex-col gap-1 justify-center">
+        <div className="py-12 px-4 lg:py-20 lg:px-8 bg-main h-screen md:flex md:justify-center md:items-center ">
+            <div className='hidden md:block'>
+                <img src={image} className='md:max-w-sm lg:max-w-md'/>
+            </div>
+           <div className="flex flex-col gap-1 lg:gap-2 justify-center md:justify-start w-full md:w-1/2 ">
                 <div>
-                    <span className="text-2xl font-semibold">Masuk ke</span>
-                    <span className="text-2xl font-bold"> OWL.</span>
+                    <span className="text-2xl lg:text-4xl font-semibold">Masuk ke</span>
+                    <span className="text-2xl lg:text-4xl font-bold"> OWL.</span>
                 </div>
-                <p className="text-base">Silahkan masukkan informasi akun kamu.</p>
+                <p className="text-base lg:text-lg ">Silahkan masukkan informasi akun kamu.</p>
                 <div className="flex flex-col gap-4 mt-4">
-                    <input type="text" className="border-gray-400 border rounded-md py-1 px-3 placeholder-black placeholder-opacity-70" placeholder="Email"/>
-                    <input type="password" className="border-gray-400 border rounded-md py-1 px-3 placeholder-black placeholder-opacity-70" placeholder="Password"/>
+                    <input type="text" className="border-gray-400 border rounded-md py-1 lg:py-2 px-3 placeholder-black placeholder-opacity-70" placeholder="Email"/>
+                    <input type="password" className="border-gray-400 border rounded-md py-1 lg:py-2 px-3 placeholder-black placeholder-opacity-70" placeholder="Password"/>
                     <div className="flex">
                         <input type="checkbox"/>
                         <p className="text-sm">&nbsp;Remember Me</p>
                     </div>
                 </div>
-                <button className="bg-orange-400 p-1 rounded-md font-light mt-2">Login</button>
+                <button className="bg-orange-400 p-1 rounded-md font-light mt-2 lg:mt-4 lg:text-lg">Login</button>
                 <hr className="my-4"/>
-                <button className="text-blue font-light p-1 border border-blue-300 rounded-lg bg-white text-blue-300 flex justify-center items-center gap-2">
+                <button className="text-blue font-light p-1 lg:p-2 border border-blue-300 rounded-lg bg-white text-blue-300 flex justify-center items-center gap-2">
                     <img src={googleIcon} width={20} height={20}/>
                     Login With Google
                 </button>
-                <button className="text-blue font-light p-1 border border-blue-300 rounded-lg mt-2 bg-white text-blue-300 flex justify-center items-center gap-2">
+                <button className="text-blue font-light p-1 lg:p-2 border border-blue-300 rounded-lg mt-2 bg-white text-blue-300 flex justify-center items-center gap-2">
                     <img src={githubIcon} width={20} height={20}/>Login With Github
                 </button>
                 
-                <div className="text-center text-sm mt-1">
+                <div className="text-center text-sm mt-2">
                     <span>Belum punya akun? </span>
                     <span className="text-blue-300">Daftar disini</span>
                 </div>
