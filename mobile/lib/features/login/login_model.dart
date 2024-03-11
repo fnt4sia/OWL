f56aaa9e939aa724cfbf0b13967d1079181363fd
fnt4sia <fitraramadhan668@gmail.com>
2024-03-11 11:50:45 +0700

    add: model login

---
+++ mobile/lib/features/login/login_model.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class LoginModel {
+  static Uri url = Uri.parse('https://nodejsdeployowl.et.r.appspot.com/login');
+
+  static Future<bool> loginEmail(String email, String password) async {
+    final loginResponse = await http.post(
+      url,
+      body: jsonEncode(
+        {
+          'email': email,
+          'password': password,
+        },
+      ),
+    );
+
+    if (loginResponse.statusCode != 200) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
