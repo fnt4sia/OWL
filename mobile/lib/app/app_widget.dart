6848b8e2c3034576f93d9828b725a05abfa89c58
fnt4sia <fitraramadhan668@gmail.com>
2024-03-11 10:24:35 +0700

    refactor: folder management

---
+++ mobile/lib/app/app_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'app_routes.dart';
+import '../features/login/login_page.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      initialRoute: '/login',
+      routes: {
+        AppRoutes.home: (context) => const LoginPage(),
+      },
+    );
+  }
+}
--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,27 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/Pages/login_page.dart';
+import 'app/app_widget.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      initialRoute: '/login',
-      routes: {
-        '/login': (context) => const LoginPage(),
-      },
-    );
-  }
+  runApp(const AppWidget());
 }
