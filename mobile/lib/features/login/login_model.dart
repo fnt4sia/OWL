0143f20dfd8df55a6f662eec986096f70e4d1f0a
fnt4sia <fitraramadhan668@gmail.com>
2024-03-12 12:54:07 +0700

    feat : add and check jwt

--- mobile/lib/features/login/login_model.dart
+++ mobile/lib/features/login/login_model.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginModel {
   static Uri url = Uri.parse('https://nodejsdeployowl.et.r.appspot.com/login');
@@ -17,6 +18,11 @@ class LoginModel {
     );
 
     if (loginResponse.statusCode == 200) {
+      String jwtToken =
+          jsonDecode(loginResponse.body)['session']['access_token'];
+
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString('jwt', jwtToken);
       return true;
     } else {
       return false;
