a2e8dac1eda7e2a0cdda7f412eb68eadc97fbc26
Seviko Attalarik <attalarik.handoko@gmail.com>
2024-03-15 19:24:29 +0700

    fix: auth

--- web/src/Hooks/CheckUser.js
+++ web/src/Hooks/CheckUser.js
@@ -3,32 +3,35 @@ import { createClient } from "@supabase/supabase-js";
 const anonKey = process.env.REACT_APP_ANONKEY
 const supabase = createClient('https://gkrzpruurzsarotxqmbc.supabase.co', process.env.REACT_APP_ANONKEY);
 
-const checkJWT = async (session) => {
+
+const CheckUserLoggedIn = async () => {
     try {
-        // Get the access token and refresh token from the session
+        // Get the user session
+        const { data: { user: userSession } } = await supabase.auth.getUser();
+        if (userSession) {
+            return;
+        }
+
+        // if no user, check if there is a session in the local storage and set it
+        const session = JSON.parse(localStorage.getItem('session'));
         const access_token = session.access_token;
         const refresh_token = session.refresh_token;
 
-        // Set the session from the access token and refresh token
         const { user, error } = await supabase.auth.setSession({
-            access_token
+            access_token,
+            refresh_token
         });
-
-        if(error) {
-            localStorage.removeItem('session');
-            window.location.href = '/login';
+        
+        if (error) {
+            throw error;
         }
+
+        localStorage.removeItem('session');
+        window.location.href = '/login';
+
     } catch (error) {
         localStorage.removeItem('session');
         window.location.href = '/login';
-    }
-}
-
-const CheckUserLoggedIn = () => {
-    let session = JSON.parse(localStorage.getItem('session'));
-
-    if(session == null) {
-        checkJWT(session);
     }
 }
 
