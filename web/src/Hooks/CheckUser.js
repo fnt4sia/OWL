8058e6e2dbc7108603de643a62646aba30f312b7
Seviko Attalarik <attalarik.handoko@gmail.com>
2024-03-15 19:43:01 +0700

    fix: auth

--- web/src/Hooks/CheckUser.js
+++ web/src/Hooks/CheckUser.js
@@ -1,35 +1,14 @@
-import { createClient } from "@supabase/supabase-js";
-
-const anonKey = process.env.REACT_APP_ANONKEY
-const supabase = createClient('https://gkrzpruurzsarotxqmbc.supabase.co', process.env.REACT_APP_ANONKEY);
-
+import supabase from '../middleware/supabase';
 
 const CheckUserLoggedIn = async () => {
     try {
-        // Get the user session
         const { data: { user: userSession } } = await supabase.auth.getUser();
         if (userSession) {
-            localStorage.removeItem('session');
             return;
         }
 
-        // if no user, check if there is a session in the local storage and set it
-        const session = JSON.parse(localStorage.getItem('session'));
-        const access_token = session.access_token;
-        const refresh_token = session.refresh_token;
-
-        const { user, error } = await supabase.auth.setSession({
-            access_token,
-            refresh_token
-        });
-        
-        if (error) {
-            throw error;
-        }
-
-        localStorage.removeItem('session');
+        throw new Error('No user session');
     } catch (error) {
-        localStorage.removeItem('session');
         window.location.href = '/login';
     }
 }
