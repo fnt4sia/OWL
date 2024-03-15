ea69f8ac923c439de55c7bdf258d1b49919ee19e
Seviko Attalarik <attalarik.handoko@gmail.com>
2024-03-15 20:48:36 +0700

    temp

--- web/src/middleware/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const anonKey = process.env.REACT_APP_ANONKEY;
-const supabaseUrl = 'https://gkrzpruurzsarotxqmbc.supabase.co';
-
-const supabase = createClient(supabaseUrl, anonKey);
-
-export default supabase;
