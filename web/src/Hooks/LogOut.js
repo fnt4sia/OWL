459f333865a7ad9e184a5814396252e9f9d9a2d6
Seviko Attalarik <attalarik.handoko@gmail.com>
2024-03-15 20:44:30 +0700

    fix: href

--- web/src/Hooks/LogOut.js
+++ web/src/Hooks/LogOut.js
@@ -1,11 +1,11 @@
-import supabase from '../Middleware/Supabase'
+import supabase from '../Middleware/Supabase';
 
 const LogOut = async () => {
     const { error } = await supabase.auth.signOut();
     if(error){
         throw error;
     }
-    window.location.href('/login');
+    window.location.href = '/login';
 }
 
 export default LogOut
